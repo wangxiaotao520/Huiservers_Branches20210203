7a51d2d1bf27c729c36ab04ce55bcf0521a871bd
wangxiaotao <18635423763@163.com>
2019-04-08 15:58:41 +0800

    提交新package

---
+++ app/src/main/java/com/huacheng/huiservers/ui/index/workorder_second/PublicWorkOrderCommitActivity.java
@@ -0,0 +1,45 @@
+package com.huacheng.huiservers.ui.index.workorder_second;
+
+import com.huacheng.huiservers.ui.base.BaseActivity;
+
+/**
+ * Description: 公共提交页面
+ * created by wangxiaotao
+ * 2019/4/8 0008 下午 3:44
+ */
+public class PublicWorkOrderCommitActivity extends BaseActivity{
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initIntentData() {
+
+    }
+
+    @Override
+    protected int getFragmentCotainerId() {
+        return 0;
+    }
+
+    @Override
+    protected void initFragment() {
+
+    }
+}
